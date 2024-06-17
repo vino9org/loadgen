392e99e45873ffc466775c2ffa702819f64f799c
Li Lin <guru.lin@gmail.com>
2024-06-17 13:50:41 +0800

    refactor request payload generation

--- api_tasks.py
+++ api_tasks.py
@@ -1,14 +1,13 @@
 import csv
 import random
 import string
-from datetime import datetime
-
-import pytz
+import time
+from datetime import datetime, timedelta, timezone
 
 __all_accounts__ = []
 
 
-def _read_all_accounts_(file_path: str):
+def read_accounts_from_csv(file_path: str):
     accounts = set()
     with open(file_path, "r") as csv_f:
         reader = csv.DictReader(csv_f)
@@ -23,16 +22,19 @@ def _random_characters_(length: int) -> str:
     return "".join(random.choice(characters) for _ in range(length))
 
 
-def rand_fund_transfer_request(csv_file: str):
-    global __all_accounts__
+def get_local_timezone():
+    local_tz_name = time.tzname[time.localtime().tm_isdst]  # returns something like "+08"
+    offset = timedelta(hours=int(local_tz_name))
+    return timezone(offset)
 
-    if len(__all_accounts__) == 0:
-        __all_accounts__ = _read_all_accounts_(csv_file)
 
-    debit_account = random.choice(__all_accounts__)
-    credit_account = random.choice(__all_accounts__)
-    amount = random.randint(300, 1000)
-    now_dt = datetime.now(pytz.timezone("Asia/Singapore"))
+def rand_fund_transfer_request(all_accounts: list[str], amount: int = 0):
+    debit_account = random.choice(all_accounts)
+    credit_account = random.choice(all_accounts)
+    if amount == 0:
+        amount = random.randint(300, 1000)
+
+    now_dt = datetime.now(get_local_timezone())
 
     return {
         "trx_date": now_dt.strftime("%Y-%m-%d"),
--- locustfile.py
+++ locustfile.py
@@ -1,6 +1,6 @@
 from locust import HttpUser, events, run_single_user, task
 
-from api_tasks import rand_fund_transfer_request
+from api_tasks import rand_fund_transfer_request, read_accounts_from_csv
 
 
 @events.init_command_line_parser.add_listener
@@ -19,22 +19,21 @@ def custom_wait_time_function(locust):
     return float(wait_time)
 
 
-def custom_data_file_function(locust):
-    data_file = locust.environment.parsed_options.data_file
-    return data_file
-
-
 class FundTransferApiUser(HttpUser):
     host = "http://localhost:8000"
     wait_time = custom_wait_time_function
-    data_file = custom_data_file_function
+
+    def on_start(self):
+        data_file = self.environment.parsed_options.data_file
+        self.all_accounts = read_accounts_from_csv(data_file)
 
     @task
     def call_fund_transfer_api(self):
+        payload = rand_fund_transfer_request(self.all_accounts)
         response = self.client.post(
             url="/api/casa/transfers",
             headers={"content-type": "application/json"},
-            json=rand_fund_transfer_request(self.data_file),
+            json=payload,
         )
         if response.status_code != 201:
             print(response.text)
